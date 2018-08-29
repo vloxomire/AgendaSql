4f9924af5e0df2d3bbb373bab80cfa9ca9bee1b2
Max <vloxomire@gmail.com>
2018-08-29 00:58:27 -0300

    0058

--- app/src/main/java/com/max/agendasql/Interfaz/Celdas.java
+++ app/src/main/java/com/max/agendasql/Interfaz/Celdas.java
@@ -1,13 +1,15 @@
-package com.max.agendasql;
+package com.max.agendasql.Interfaz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.max.agendasql.R;
+
+public class Celdas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.celdas);
     }
 }
---
+++ app/src/main/java/com/max/agendasql/Interfaz/MainActivity.java
@@ -0,0 +1,15 @@
+package com.max.agendasql.Interfaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.max.agendasql.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
