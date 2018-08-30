0425015a5afa8ea638b453edea7fb828deaf149a
Max <vloxomire@gmail.com>
2018-08-29 22:17:51 -0300

    2217

--- app/src/main/java/com/max/agendasql/Dao/SqliteAgenda.java
+++ app/src/main/java/com/max/agendasql/Dao/SqliteAgenda.java
@@ -1,4 +1,26 @@
 package com.max.agendasql.Dao;
 
-public class SqliteAgenda {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqliteAgenda extends SQLiteOpenHelper{
+    private static final String DBNAME="dbAgenda";
+    private static final Integer DBVERSION=1;
+    private Context context;
+    private SQLiteDatabase conexion;
+
+    public SqliteAgenda(Context context) {
+        super(context, DBNAME, null, DBVERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
