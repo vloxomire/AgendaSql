527fde8f924e671bbd1a154d37d440a88ef57f27
Max "Destroyer" Sebastian Saldaña <40814622+vloxomire@users.noreply.github.com>
2018-09-14 15:02:20 -0300

    AdapterRecycler15:01pm
    falta:
     public void onBindViewHolder, getItemCount,  private class AgendaViewHolder

--- app/src/main/java/com/max/agendasql/Adapters/AdapterRecycler.java
+++ app/src/main/java/com/max/agendasql/Adapters/AdapterRecycler.java
@@ -1,4 +1,23 @@
 package com.max.agendasql.Adapters;
 
-public class AdapterRecycler {
+public class AdapterRecycler extends RecyclerView.Adapter{
+  private Context context;
+  private Arraylist<Agenda>agendaArraylist;
+  private ListenerRecycler listenerRecycler;
+  
+  public AdapterRecyler(Context context,Arraylist<Agenda> agendaArraylist){
+    this.context=context;
+    this.agendaArraylist=agendaArraylist;
+  }
+  @NoNull
+  @Override
+  publicRecyclerView.ViewHolder onCreateViewHolder(@NoNull ViewGroup vGRecycler,int posicion){
+    LayoutInflater inflaterRecycler=LayoutInflater.from(context);
+    View viewRecycler=inflaterRecycler.inflate(R.layout.celdas,parent,false);
+    viewRecycler.setOnClickListener(listenerRecycler);
+    
+    AgendaViewHolder agendaViewHolder=new AgendaViewHolder(viewRecycler);
+    
+    return agendaViewHolder;
+  }
 }
