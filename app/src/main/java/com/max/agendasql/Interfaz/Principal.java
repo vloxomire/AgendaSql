0da543dcb58a310c9e020a4a4513fff5daba11fa
Max "Destroyer" Sebastian Saldaña <40814622+vloxomire@users.noreply.github.com>
2018-09-14 14:25:21 -0300

    btnListener14:25pm-14/09/2018
    BotonListener:Instanciado, vinculadon y seteado
    ListenerRegistrar:Creo el objeto

--- app/src/main/java/com/max/agendasql/Interfaz/Principal.java
+++ app/src/main/java/com/max/agendasql/Interfaz/Principal.java
@@ -8,7 +8,7 @@ import com.max.agendasql.Listeners.ListenerVerRegistro;
 import com.max.agendasql.R;
 
 public class Principal extends AppCompatActivity {
-private Button botonRegistrarX,botonVerListadoX;
+private Button botonRegistrarX,botonVerListadoX,botonRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,15 @@ private Button botonRegistrarX,botonVerListadoX;
         setContentView(R.layout.activity_principal);
         botonRegistrarX=findViewById(R.id.botonRegistrar);
         botonVerListadoX=findViewById(R.id.botonVerListado);
-
+        botonRecycler=findViewById(R.id.botonRecycler);
+        //new(<INVESTIGAR>invoca al constructor de la clase,reserva memoria, y obtiene la direccion de memoria donde esta
+        //almacenado el objetoy se habilita para cargar dato) instancia la memoria lo q tengo como esqueleto en la clase)
         ListenerRegistrar listenerRegistrar=new ListenerRegistrar(this);
         ListenerVerRegistro listenerVerRegistro=new ListenerVerRegistro(this);
+        ListenerRecycler listenerRecycler=new ListenerRecycler(this);
 
         botonRegistrarX.setOnClickListener(listenerRegistrar);
         botonVerListadoX.setOnClickListener(listenerVerRegistro);
+        botonRecycler.setOnClickListener(listenerRecycler);
     }
 }
