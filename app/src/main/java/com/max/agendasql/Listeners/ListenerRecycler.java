97f3a9759453174eae4c95cc214305b1569264dd
Max "Destroyer" Sebastian Saldaña <40814622+vloxomire@users.noreply.github.com>
2018-09-14 13:42:29 -0300

    ListenerRecycler01:35pm

--- app/src/main/java/com/max/agendasql/Listeners/ListenerRecycler.java
+++ app/src/main/java/com/max/agendasql/Listeners/ListenerRecycler.java
@@ -1,4 +1,14 @@
 package com.max.agendasql.Listeners;
 
-public class ListenerRecycler {
+public class ListenerRecycler implements View.OnClickListener{
+  Principal context;
+  
+  Public ListenerRecycler(Principal context){
+    this.context=context;
+  }
+ @Override
+  public void onClick(View vRecycler){
+    Intent intentRecycler= new Intent(context,Recyler.class);
+    context.startActivity(intentRecycler);
+  }
 }
