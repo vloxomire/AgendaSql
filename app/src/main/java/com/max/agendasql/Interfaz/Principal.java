0ff5a0f78e92aeef6159d0c59f5744c5f4b32a28
Max <vloxomire@gmail.com>
2018-09-15 22:19:10 -0300

    2219pm

--- app/src/main/java/com/max/agendasql/Interfaz/Principal.java
+++ app/src/main/java/com/max/agendasql/Interfaz/Principal.java
@@ -1,9 +1,11 @@
 package com.max.agendasql.Interfaz;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.max.agendasql.Listeners.ListenerFabRegistrar;
 import com.max.agendasql.Listeners.ListenerRecycler;
 import com.max.agendasql.Listeners.ListenerRegistrar;
 import com.max.agendasql.Listeners.ListenerVerRegistro;
@@ -11,6 +13,7 @@ import com.max.agendasql.R;
 
 public class Principal extends AppCompatActivity {
 private Button botonRegistrarX,botonVerListadoX,botonRecycler;
+private FloatingActionButton fabRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +22,17 @@ private Button botonRegistrarX,botonVerListadoX,botonRecycler;
         botonRegistrarX=findViewById(R.id.botonRegistrar);
         botonVerListadoX=findViewById(R.id.botonVerListado);
         botonRecycler=findViewById(R.id.botonRecycler);
+        fabRegistrar=findViewById(R.id.fab);
         //new(<INVESTIGAR>invoca al constructor de la clase,reserva memoria, y obtiene la direccion de memoria donde esta
         //almacenado el objetoy se habilita para cargar dato) instancia la memoria lo q tengo como esqueleto en la clase)
         ListenerRegistrar listenerRegistrar=new ListenerRegistrar(this);
         ListenerVerRegistro listenerVerRegistro=new ListenerVerRegistro(this);
         ListenerRecycler listenerRecycler=new ListenerRecycler(this);
+        ListenerFabRegistrar listenerFabRegistrar=new ListenerFabRegistrar(this);
 
         botonRegistrarX.setOnClickListener(listenerRegistrar);
         botonVerListadoX.setOnClickListener(listenerVerRegistro);
         botonRecycler.setOnClickListener(listenerRecycler);
+        fabRegistrar.setOnClickListener(listenerFabRegistrar);
     }
 }
---
+++ app/src/main/java/com/max/agendasql/Listeners/ListenerFabRegistrar.java
@@ -0,0 +1,25 @@
+package com.max.agendasql.Listeners;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.max.agendasql.Interfaz.Activity2;
+import com.max.agendasql.Interfaz.Principal;
+
+public class ListenerFabRegistrar implements View.OnClickListener {
+    private Principal contextFabReg;
+
+    public ListenerFabRegistrar(Principal contextFabReg) {
+        this.contextFabReg = contextFabReg;
+    }
+
+    @Override
+    public void onClick(View vFabRgt) {
+        Toast.makeText(contextFabReg, "Fab,BotonFlotante>Registrar", Toast.LENGTH_SHORT).show();
+        Intent intentfabRgt=new Intent(contextFabReg, Activity2.class);
+        intentfabRgt.putExtra("ID",0);
+        contextFabReg.startActivity(intentfabRgt);
+    }
+}
